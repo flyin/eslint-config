8614da783acd837065adc92f0cff1d1ed585d566
flyin <alex@alex-web.ru>
2020-03-20 09:29:34 +0300

    update packages

--- react.js
+++ react.js
@@ -17,14 +17,14 @@ module.exports = {
     rules: {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
-        'react/destructuring-assignment': [1, 'never', {'ignoreClassFields': true}],
+        'react/destructuring-assignment': [1, 'always', {'ignoreClassFields': true}],
         'react/jsx-curly-spacing': [1, 'never'],
-        'react/jsx-uses-react': 2,
+        'react/jsx-uses-react': 0,
         'react/jsx-uses-vars': 1,
         'react/prefer-stateless-function': 2,
         'react/jsx-no-bind': 2,
         'react/self-closing-comp': 2,
-        'react/jsx-handler-names': 0,
+        'react/jsx-handler-names': 1,
     },
 
     settings: {
